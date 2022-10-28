c054af6264cccd3df378747c249f9ebcf4fc75f1
Hakan ÖZER <hakanozer02@gmail.com>
2022-10-28 10:00:19 +0300

    Project Start

---
+++ src/main/java/com/works/PaycoreSpringTest1Application.java
@@ -0,0 +1,13 @@
+package com.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaycoreSpringTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaycoreSpringTest1Application.class, args);
+    }
+
+}
